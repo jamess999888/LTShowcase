b1126bf22e3552fea69b98960f5c339d06b78559
James McGinnis <mcginnis.james.j@gmail.com>
2020-08-31 19:27:18 -0500

    update usage error in python script

--- photo-album.py
+++ photo-album.py
@@ -1,6 +1,9 @@
 import requests
 import sys
 
-ploads = {'album':sys.argv[1]}
-r = requests.get('http://localhost:49160/',params=ploads)
-print(r.text)
+if (len(sys.argv) == 2):
+    ploads = {'album':sys.argv[1]}
+    r = requests.get('http://localhost:49160/',params=ploads)
+    print(r.text)
+else:
+    print("Usage: a single int argument must be supplied")
